34baf91970a56dc75a7911c12cf3732e88ae9f74
mrmalvicino <mrmalvicino@gmail.com>
2023-06-11 19:05:55 -0300

    Fase de expedicion

--- funciones.cpp
+++ funciones.cpp
@@ -2,10 +2,30 @@
 #include <random>
 using namespace std;
 #include "funciones.h"
+/*
+bool arena_cangrejo(){
 
-int turno_nuevo(int turno_actual){
+}
+
+bool tierra_hormiga(){
+    
+}
+
+bool agua_medusa(){
+    
+}
+
+bool aire_aguila(){
+    
+}
+
+bool fuego_salamandra(){
+    
+}*/
+
+int turno_nuevo(int turno_actual, int cant_jugs){
     int turno_nuevo;
-    int const CANT_JUGADORES = 2;
+    int const CANT_JUGADORES = cant_jugs;
 
     if(turno_actual < CANT_JUGADORES){
         turno_nuevo = turno_actual + 1;
@@ -19,7 +39,8 @@ int turno_nuevo(int turno_actual){
 string nombre_de_estatuilla(int numero_de_estat){
     // Devuelve el nombre de una de las estatuillas.
 
-    string nombres_de_estat[5] =
+    int const CANT_ESTATUILLAS = 5;
+    string nombres_de_estat[CANT_ESTATUILLAS] =
     {"Estatuilla de arena: Cangrejo",
     "Estatuilla de tierra: Hormiga",
     "Estatuilla de agua: Medusa",
@@ -39,15 +60,17 @@ void mostrar_inventario(string jugadores_fase_exp[], int cant_jugs, int estatuil
         cout << "Inventario de " << jugadores_fase_exp[i] << ":" << endl;
 
         for(int j = 0; j < CANT_ESTATUILLAS; j++){
-            if(estatuillas[j] == 1){
+            if(estatuillas[j] == i + 1){
                 cout << "- " << nombre_de_estatuilla(j) << endl;
             }
         }
     }
 }
 
-void elegir_estatuilla(int estatuillas[], int CANT_ESTATUILLAS){
+void elegir_estatuillas_disponibles(int estatuillas[], int cant_ests){
     // Mostrar las estatuillas disponibles para elegir como objetivo durante fase de expedicion.
+
+    int CANT_ESTATUILLAS = cant_ests;
 
     cout << "Elegir estatuilla objetivo:" << endl;
 
@@ -61,29 +84,32 @@ void elegir_estatuilla(int estatuillas[], int CANT_ESTATUILLAS){
 int tirar_dado(int cant_caras){
     // Devuelve un numero aleatorio con probabilidad 1/caras.
 
-    int const MIN = 1;
-    int const MAX = cant_caras;
+    int const CANT_CARAS = cant_caras;
+    int numeros[CANT_CARAS];
 
-    std::random_device rndm;
-    std::mt19937 generator(rndm());
-    std::uniform_int_distribution<int> distribution(MIN, MAX);
+    for(int i = 0; i < CANT_CARAS; i++){
+      numeros[i] = i + 1;
+    }
 
-    return distribution(generator);
+    int indice = rand() % 10;
+    int aleatorio = numeros[indice];
+
+    return aleatorio;
 }
 
 int jugador_inicial_exp(){
     // Determina el jugador que comienza la fase de expedicion.
 
     int jugador_inicial;
-    int dado1 = 0;
-    int dado2 = 0;
+    int dado_jugador_1 = 0;
+    int dado_jugador_2 = 0;
 
-    while(dado1 == dado2){
-        dado1 = tirar_dado(10);
-        dado2 = tirar_dado(10);
+    while(dado_jugador_1 == dado_jugador_2){
+        dado_jugador_1 = tirar_dado(10);
+        dado_jugador_2 = tirar_dado(10);
     }
 
-    if(dado1 < dado2){
+    if(dado_jugador_1 < dado_jugador_2){
         jugador_inicial = 1;
     } else{
         jugador_inicial = 2;
@@ -92,9 +118,10 @@ int jugador_inicial_exp(){
     return jugador_inicial;
 }
 
-bool fin_expedicion(int estatuillas[], int CANT_ESTATUILLAS){
+bool fin_expedicion(int estatuillas[], int cant_ests){
     // Determina si se termina la fase de expedicion.
 
+    int CANT_ESTATUILLAS = cant_ests;
     bool fin = true;
 
     for(int i = 0; i < CANT_ESTATUILLAS; i++){
@@ -110,7 +137,7 @@ void fase_expedicion(string jugadores_menu[], int cant_jugs){
     // Jugar fase de expedicion.
 
     int const CANT_ESTATUILLAS = 5;
-    int estatuillas[CANT_ESTATUILLAS] = {};
+    int estatuillas[CANT_ESTATUILLAS] = {0,1,0,1,2}; // Array que tiene en cada componente a quien pertenece la estatuilla o tiene cero si nadie la tiene
     
     int const CANT_JUGADORES = cant_jugs;
     int objetivos[CANT_JUGADORES];
@@ -121,17 +148,36 @@ void fase_expedicion(string jugadores_menu[], int cant_jugs){
     }
 
     int turno_actual = jugador_inicial_exp();
+    int dado1;
+    int dado2;
+    int dado3;
 
     while(fin_expedicion(estatuillas, CANT_ESTATUILLAS) == 0){
         mostrar_inventario(jugadores_fase_exp, CANT_JUGADORES, estatuillas, CANT_ESTATUILLAS);
 
-        for(int i = 0; i < CANT_JUGADORES; i++){
-            cout << "Turno de " << jugadores_fase_exp[turno_actual-1] << endl;
-            elegir_estatuilla(estatuillas, CANT_ESTATUILLAS);
-            cin >> objetivos[turno_actual-1];
-            turno_actual = turno_nuevo(turno_actual);
+        for(int i = 0; i < CANT_JUGADORES; i++){ // Jugadores eligen objetivos
+            cout << "Turno de " << jugadores_fase_exp[turno_actual - 1] << endl;
+            elegir_estatuillas_disponibles(estatuillas, CANT_ESTATUILLAS);
+            cin >> objetivos[turno_actual - 1];
+            turno_actual = turno_nuevo(turno_actual, CANT_JUGADORES);
         }
 
+        for(int i = 0; i < CANT_JUGADORES; i++){ // Jugadores tiran dados para conseguir estatuillas objetivo
+            cout << "Turno de " << jugadores_fase_exp[turno_actual - 1] << endl;
+            
+            if(estatuillas[4] == 0 || estatuillas[4] == turno_actual){ // Si nadie tiene la salamandra o la tiene el del turno, este tira dos dados
+                dado1 = tirar_dado(10);
+                dado2 = tirar_dado(10);
+                cout << jugadores_fase_exp[turno_actual-1] << " tiro los dados " << dado1 << " y " << dado2 << endl;
+            } else if(estatuillas[4] != 0 && estatuillas[4] != turno_actual){ // Si otro tiene la salamandra el del turno tira 3 dados
+                dado1 = tirar_dado(10);
+                dado2 = tirar_dado(10);
+                dado3 = tirar_dado(10);
+                cout << jugadores_fase_exp[turno_actual-1] << " tiro los dados " << dado1 << ", " << dado2 << " y " << dado3 << endl;
+            }
+
+            turno_actual = turno_nuevo(turno_actual, CANT_JUGADORES);
+        }
 
         // Jugador 1 tira 2 dados
         // informar si gano y actualizar vector estatuillas()
--- funciones.h
+++ funciones.h
@@ -1,15 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int turno_nuevo(int turno_actual);
+int turno_nuevo(int turno_actual, int cant_jugs);
+
 string nombre_de_estatuilla(int numero_de_estat);
-void mostrar_inventario(string jugador1, string jugador2, int estatuillas[], int CANT_EST);
-void elegir_estatuilla(int estatuillas[], int CANT_EST);
-int tirar_dado(int caras);
+
+void mostrar_inventario(string jugadores_fase_exp[], int cant_jugs, int estatuillas[], int cant_ests);
+
+void elegir_estatuillas_disponibles(int estatuillas[], int cant_ests);
+
+int tirar_dado(int cant_caras);
+
 int jugador_inicial_exp();
-bool fin_expedicion(int estatuillas[], int CANT_EST);
-void fase_expedicion(string jugadores[], int cant_jugadores);
-void menu_jugar(int cant_jugadores);
+
+bool fin_expedicion(int estatuillas[], int cant_ests);
+
+void fase_expedicion(string jugadores_menu[], int cant_jugs);
+
+void menu_jugar(int cant_jugs);
+
 void menu_estadisticas();
+
 void menu_creditos();
+
 void menu();
